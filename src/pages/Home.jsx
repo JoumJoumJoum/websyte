5eee02af16458d27a542182077f8379458940784
Jotham <jotham0303@gmail.com>
2025-10-10 18:41:56 +0530

    removed manu

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -127,7 +127,7 @@ const Home = () => {
           <p>Join our chapter, participate in events, and collaborate on real-world geospatial challenges.</p>
           <div style={{marginTop:16}}>
             <a
-              href="google.com"
+              href="www.google.com"
               target="_blank"
               rel="noopener noreferrer"
               className="michroma"
