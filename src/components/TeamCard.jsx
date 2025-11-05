07dc0cb94e3f5d60129d62d891b28b5f6dbd1fd0
Jotham <jotham0303@gmail.com>
2025-11-06 03:36:21 +0530

    Card changes to team members

--- src/components/TeamCard.jsx
+++ src/components/TeamCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import SpotlightCard from './SpotlightCard';
 import './TeamCard.css';
 
-export default function TeamCard({ name, img, role = 'Position Title', description = '1–2 lines describing this member, their focus, or contribution.', objectPosition = 'center 50%' }) {
+export default function TeamCard({ name, img, role = 'Position Title', linkedin = 'https://www.google.com', objectPosition = 'center 50%' }) {
   return (
     <SpotlightCard className="team-card">
       <div className="team-card__image-wrap">
@@ -11,7 +11,11 @@ export default function TeamCard({ name, img, role = 'Position Title', descripti
       <div className="team-card__info">
         <h3 className="michroma team-card__name">{name}</h3>
         <div className="team-card__role">{role}</div>
-        <p className="team-card__desc">{description}</p>
+        <a href={linkedin} target="_blank" rel="noopener noreferrer" className="team-card__linkedin">
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+            <path fill="currentColor" d="M19 3a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h14m-.5 15.5v-5.3a3.26 3.26 0 0 0-3.26-3.26c-.85 0-1.84.52-2.32 1.3v-1.11h-2.79v8.37h2.79v-4.93c0-.77.62-1.4 1.39-1.4a1.4 1.4 0 0 1 1.4 1.4v4.93h2.79M6.88 8.56a1.68 1.68 0 0 0 1.68-1.68c0-.93-.75-1.69-1.68-1.69a1.69 1.69 0 0 0-1.69 1.69c0 .93.76 1.68 1.69 1.68m1.39 9.94v-8.37H5.5v8.37h2.77z"/>
+          </svg>
+        </a>
       </div>
     </SpotlightCard>
   );
--- src/members/index.js
+++ src/members/index.js
@@ -2,16 +2,16 @@
 // Each member has a display name and an image URL served from /members
 
 const members = [
-  { name: 'Adith', img: '/members/adithgrss.jpg', role: 'President', description: 'President of GRSS' },
-  { name: 'Anshul', img: '/members/anshulgrss.jpg', role: 'Vice President', description: 'Vice President of GRSS' },
-  { name: 'Farha', img: '/members/farhagrss.jpg', role: 'Managing Director', description: 'Managing Director of GRSS' },
-  { name: 'Jotham', img: '/members/jothamgrss.jpg', role: 'Technical Director', description: 'Technical Director of GRSS' },
-  { name: 'Kaushik', img: '/members/kaushikgrss.jpg', role: 'Technical Advisor', description: 'Technical Advisor of GRSS' },
-  { name: 'Lekshmi', img: '/members/lachugrss.jpg', role: 'Creative Director', description: 'Creative Director of GRSS' },
-  { name: 'Manu', img: '/members/manugrss.jpg', role: 'HR Director', description: 'HR Director of GRSS' },
-  { name: 'Mehul', img: '/members/mehulgrss.jpg', role: 'Treasurer', description: 'Treasurer of GRSS' },
-  { name: 'Shagun', img: '/members/shagungrss.jpg', role: 'General Secretary', description: 'General Secretary of GRSS' },
-  { name: 'Yadvendra Singh', img: '/members/ypsirgrss.jpg', role: 'Faculty Director', description: 'Faculty Director of GRSS' }
+  { name: 'Yadvendra Singh', img: '/members/ypsirgrss.jpg', role: 'Faculty Director', linkedin: 'https://www.linkedin.com/in/dr-yadvendra-pratap-singh-544812139' },
+  { name: 'Adith', img: '/members/adithgrss.jpg', role: 'President', linkedin: 'https://www.linkedin.com/in/adith-nambiar-8889341b6' },
+  { name: 'Anshul', img: '/members/anshulgrss.jpg', role: 'Vice President', linkedin: 'https://www.linkedin.com/in/anshul-anand-b887401a0' },
+  { name: 'Farha', img: '/members/farhagrss.jpg', role: 'Managing Director', linkedin: 'https://www.google.com' },
+  { name: 'Jotham', img: '/members/jothamgrss.jpg', role: 'Technical Director', linkedin: 'https://www.linkedin.com/in/jotham-travasso-21b52b2b0' },
+  { name: 'Kaushik', img: '/members/kaushikgrss.jpg', role: 'Technical Advisor', linkedin: 'https://www.google.com' },
+  { name: 'Lekshmi', img: '/members/lachugrss.jpg', role: 'Creative Director', linkedin: 'https://www.linkedin.com/in/lekshminaji' },
+  { name: 'Manu', img: '/members/manugrss.jpg', role: 'HR Director', linkedin: 'https://www.google.com' },
+  { name: 'Mehul', img: '/members/mehulgrss.jpg', role: 'Treasurer', linkedin: 'https://www.linkedin.com/in/mehul-garg-584a641b7' },
+  { name: 'Shagun', img: '/members/shagungrss.jpg', role: 'General Secretary', linkedin: 'https://www.linkedin.com/in/shagun-singh-915b7b297' }
 ];
 
 export default members;
