e2c2ab0a85a50693be43f45319d6d0e7e61abf1e
Jotham <jotham0303@gmail.com>
2025-11-06 10:12:06 +0530

    Added everyones linkedin

--- src/members/index.js
+++ src/members/index.js
@@ -2,14 +2,14 @@
 // Each member has a display name and an image URL served from /members
 
 const members = [
-  { name: 'Yadvendra Singh', img: '/members/ypsirgrss.jpg', role: 'Faculty Director', linkedin: 'https://www.linkedin.com/in/dr-yadvendra-pratap-singh-544812139' },
+  { name: 'Yadvendra Singh', img: '/members/ypsirgrss.jpg', role: 'Faculty Coordinator', linkedin: 'https://www.linkedin.com/in/dr-yadvendra-pratap-singh-544812139' },
   { name: 'Adith', img: '/members/adithgrss.jpg', role: 'President', linkedin: 'https://www.linkedin.com/in/adith-nambiar-8889341b6' },
   { name: 'Anshul', img: '/members/anshulgrss.jpg', role: 'Vice President', linkedin: 'https://www.linkedin.com/in/anshul-anand-b887401a0' },
-  { name: 'Farha', img: '/members/farhagrss.jpg', role: 'Managing Director', linkedin: 'https://www.google.com' },
+  { name: 'Farha', img: '/members/farhagrss.jpg', role: 'Managing Director', linkedin: 'https://www.linkedin.com/in/farha-sherani' },
   { name: 'Jotham', img: '/members/jothamgrss.jpg', role: 'Technical Director', linkedin: 'https://www.linkedin.com/in/jotham-travasso-21b52b2b0' },
-  { name: 'Kaushik', img: '/members/kaushikgrss.jpg', role: 'Technical Advisor', linkedin: 'https://www.google.com' },
+  { name: 'Kaushik', img: '/members/kaushikgrss.jpg', role: 'Technical Advisor', linkedin: 'https://www.linkedin.com/in/s-kaushik-rao-33336a289?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app' },
   { name: 'Lekshmi', img: '/members/lachugrss.jpg', role: 'Creative Director', linkedin: 'https://www.linkedin.com/in/lekshminaji' },
-  { name: 'Manu', img: '/members/manugrss.jpg', role: 'HR Director', linkedin: 'https://www.google.com' },
+  { name: 'Manu', img: '/members/manugrss.jpg', role: 'HR Director', linkedin: 'https://www.linkedin.com/in/manu-sankar-k-m-b172a5324?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app' },
   { name: 'Mehul', img: '/members/mehulgrss.jpg', role: 'Treasurer', linkedin: 'https://www.linkedin.com/in/mehul-garg-584a641b7' },
   { name: 'Shagun', img: '/members/shagungrss.jpg', role: 'General Secretary', linkedin: 'https://www.linkedin.com/in/shagun-singh-915b7b297' }
 ];
